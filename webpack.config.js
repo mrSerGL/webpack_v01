054bbf1eee4eeb51a172009363be66c23ec9798a
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-03-07 09:31:19 +0200

    curr

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(
-                path.resolve(__dirname, "public"),
-                "index.html"
-            ),
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-        // new ESLintPlugin()
-    ],
-
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "public"),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        compress: true,
-        port: 5000,
-    },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(path.resolve(__dirname, 'public'), 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    // new ESLintPlugin()
+  ],
+
+  devServer: {
+    port: 5000,
+    // open: true,
+    // stats: 'errors-only',
+  },
 };
